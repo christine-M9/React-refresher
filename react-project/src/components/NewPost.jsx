227c5ba79830fe7a985ea9675a12d50f7a5bf7e6
Christine <christinekhekama99@gmail.com>
2025-02-04 08:59:20 +0300

    working with state and updating it

--- react-project/src/components/NewPost.jsx
+++ react-project/src/components/NewPost.jsx
@@ -1,17 +1,20 @@
+import {useState} from 'react';
 import classes from './NewPost.module.css';
 
-function NewPost(){
+function NewPost(){  
+  const [ enteredBody, setEnteredBody] = useState('');
 
   function changeBodyHandler(event){
-    console.log(event.target.value);
+    setEnteredBody(event.target.value);
   }
 
-    return(
+  return(
     <form className={classes.form}>
        <p>
         <label htmlFor="body">Text</label>
         <textarea id= "body" required rows={3} onChange={changeBodyHandler}/>
         </p> 
+        <p>{enteredBody}</p>
         <p>
         <label htmlFor="name">Your name</label> 
         <input type="text" id="name" required />
