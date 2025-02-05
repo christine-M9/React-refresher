210bb27afcda58ea0755d561033e35aa0f6a986d
Christine <christinekhekama99@gmail.com>
2025-02-05 15:20:54 +0300

    Handling form submission

--- react-project/src/components/PostsList.jsx
+++ react-project/src/components/PostsList.jsx
@@ -6,33 +6,18 @@ import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList({isPosting, onStopPosting}){
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
-
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
+ 
     return(
       <>
       {isPosting && (
          <Modal onClose={onStopPosting}>
-        <Newpost 
-        onBodyChange={bodyChangeHandler} 
-        onAuthorChange={authorChangeHandler}
-        onCancel={onStopPosting} 
+        <Newpost onCancel={onStopPosting} 
         />
         </Modal>
-        )};
-      
+        )}  
         <ul className={classes.posts}>
-          <Post author={enteredAuthor} body={enteredBody} />
-          <Post author="Khekame" body="Check out the full course!" />  
+          <Post author="Christine" body="ReactJS is fun!" />
+          <Post author="Khekame" body="Check out the full course!" />   
         </ul>
       </>  
     )
