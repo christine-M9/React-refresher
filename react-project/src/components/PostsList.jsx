c49797a842d9913ff2224369fbf3355659caf04b
Christine <christinekhekama99@gmail.com>
2025-02-05 15:51:09 +0300

    outputing list data using map

--- react-project/src/components/PostsList.jsx
+++ react-project/src/components/PostsList.jsx
@@ -8,7 +8,7 @@ import classes from './PostsList.module.css';
 function PostsList({isPosting, onStopPosting}){
    const [posts, setPosts] = useState([]);
 
-   function addPosthandler(){
+   function addPosthandler(postData){
      setPosts((existingPosts) => [postData, ...existingPosts]);
 }
 
@@ -19,10 +19,9 @@ function PostsList({isPosting, onStopPosting}){
         <Newpost onCancel={onStopPosting}  onAddPost={addPosthandler}
         />
         </Modal>
-        )}  
+        )} 
         <ul className={classes.posts}>
-          <Post author="Christine" body="ReactJS is fun!" />
-          <Post author="Khekame" body="Check out the full course!" />   
+          {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}/>)}
         </ul>
       </>  
     )
