d2752d11a48be25d4d882754de207c0b2857ef67
Christine <christinekhekama99@gmail.com>
2025-02-11 09:22:27 +0300

    sending http request

--- react-project/src/components/PostsList.jsx
+++ react-project/src/components/PostsList.jsx
@@ -1,40 +1,49 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-import Post from './Post';
-import Newpost from './NewPost';
-import Modal from './Modal';
-import classes from './PostsList.module.css';
+import Post from "./Post";
+import Newpost from "./NewPost";
+import Modal from "./Modal";
+import classes from "./PostsList.module.css";
 
-function PostsList({isPosting, onStopPosting}){
-   const [posts, setPosts] = useState([]);
+function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
 
-   function addPosthandler(postData){
-     setPosts((existingPosts) => [postData, ...existingPosts]);
-}
-
-    return(
-      <>
+  function addPosthandler(postData) {
+    fetch("http://localhost:3030/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+      })
+      .catch((error) => console.error("Error:", error));
+  }
+  return (
+    <>
       {isPosting && (
-         <Modal onClose={onStopPosting}>
-        <Newpost onCancel={onStopPosting}  onAddPost={addPosthandler}
-        />
+        <Modal onClose={onStopPosting}>
+          <Newpost onCancel={onStopPosting} onAddPost={addPosthandler} />
         </Modal>
-        )}
-        {posts.length > 0 && (
-          <ul className={classes.posts}>
+      )}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
           {posts.map((post) => (
-            <Post key={post.body} author={post.author} body={post.body}/>
-        ))} 
-         </ul>
-        )}
-        {posts.length === 0 && (
-          <div style={{textAlign: 'center', color:'white'}}>
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet. </h2>
           <p>Start adding some</p>
-          </div>
-        )}      
-      </>  
-    );
+        </div>
+      )}
+    </>
+  );
 }
 
 export default PostsList;
