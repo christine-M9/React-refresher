ea084ca9663dad3b9192d8346f327a5cf42ee789
Christine <christinekhekama99@gmail.com>
2025-02-14 11:57:20 +0300

    data fetching via loader()

--- react-project/src/routes/Posts.jsx
+++ react-project/src/routes/Posts.jsx
@@ -14,3 +14,8 @@ function Posts() {
 }
 
 export default Posts;
+export async function loader() {
+  const response = await fetch("http://localhost:3030/posts");
+  const resData = await response.json();
+  return resData.posts;
+}
